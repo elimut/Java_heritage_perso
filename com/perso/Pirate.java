5088fa59120d75773c0fc8b761e445cee3bbfa20
elimut <vandingenenannesophie@gmail.com>
2023-05-17 16:00:10 +0200

    Initial commit

---
+++ com/perso/Pirate.java
@@ -0,0 +1,23 @@
+package com.perso;
+
+public class Pirate {
+    private String name;
+    private int posX;
+    private int posY;
+
+    public Pirate(String name)
+    {
+        this.name = name;
+        this.posX = 30;
+        this.posY = 30;
+        // position, 30px
+    }
+
+    public void move(int x, int y)
+    {
+        this.posX = x;
+        this.posY = y;
+        System.out.println("Le pirate se déplace en x" + this.posX + "et en y" +this.posY);
+    }
+    
+}
