1cabfb3b3f378f0ef8838a8d8c434c66e1a89687
elimut <vandingenenannesophie@gmail.com>
2023-05-19 16:20:00 +0200

    maj

--- com/environnement/Vehicule.java
+++ com/environnement/Vehicule.java
@@ -1,10 +1,11 @@
 package com.environnement;
 
 abstract public class Vehicule {
-
+// mot clef abstract qui définit la classe abstraite
     protected String speed;
 
     abstract public void deplacer();
+    // méthode abstraite, non implémentée
     abstract public void deplacerXY(int x, int y);
-
+        // méthode abstraite, non implémentée
 }
