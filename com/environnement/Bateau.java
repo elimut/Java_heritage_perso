8c1a9c2935d8b156f84bffac963944785ce68ee8
elimut <121231529+elimut@users.noreply.github.com>
2023-05-23 09:34:34 +0200

    maj

--- com/environnement/Bateau.java
+++ com/environnement/Bateau.java
@@ -10,6 +10,7 @@ public class Bateau extends Vehicule{
     } 
     public void deplacer(){
         System.out.println("Je me déplace");
+    // récupération de la méthode abstraite de la classe Vehicule, et implémentation => devient concrète
     }
     public void deplacerXY(int x, int y){
         System.out.println("Je me déplace de " + x + y);
